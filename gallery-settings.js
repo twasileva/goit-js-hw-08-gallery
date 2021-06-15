019be4376427529c4591b118692bc409a75b9117
[twasileva] <[twasileva@gmail.com]>
2021-06-16 01:15:37 +0300

    реализовано закрытие по модалке + закрытие по esc

--- gallery-settings.js
+++ gallery-settings.js
@@ -3,13 +3,17 @@ import defaultExport from './gallery-items.js'
 
 const galleryContainer = document.querySelector('.js-gallery')
 const isModal = document.querySelector('.js-lightbox')
+const isOverlay = document.querySelector('.lightbox__overlay')
 const isModalImage = document.querySelector('.lightbox__image')
 const buttonCloseModal = document.querySelector('button[data-action="close-lightbox"]')
-const imagesMarkup = createGalleryImages(defaultExport)
 
+const imagesMarkup = createGalleryImages(defaultExport)
 galleryContainer.insertAdjacentHTML('beforeend', imagesMarkup)
+
 galleryContainer.addEventListener('click', onGalleryImageClick)
+isOverlay.addEventListener('click', onOverlayModalClick)
 buttonCloseModal.addEventListener('click', onBtnCloseModalClick)
+
 
 function createGalleryImages(images) {
   return images.map(({ preview, original, description }) => {
@@ -33,7 +37,7 @@ function createGalleryImages(images) {
 
 function onGalleryImageClick(e) {
   e.preventDefault()
-
+  window.addEventListener('keydown', onEscCloseModalClick)
   const isGalleryImageEl = e.target.classList.contains('gallery__image')
   if (!isGalleryImageEl) {
     return
@@ -42,9 +46,22 @@ function onGalleryImageClick(e) {
   const originalSizeImage = e.target.dataset.source
   isModal.classList.add('is-open')
   isModalImage.src = originalSizeImage
+
+}
+
+function onOverlayModalClick(e) {
+  onBtnCloseModalClick()
 }
 
 function onBtnCloseModalClick(e) {
+  window.removeEventListener('keydown', onEscCloseModalClick)
   isModal.classList.remove('is-open')
   isModalImage.src = ''
 }
+
+function onEscCloseModalClick(e) {
+  if (e.code === 'Escape') {
+    onBtnCloseModalClick()
+  }
+}
+
